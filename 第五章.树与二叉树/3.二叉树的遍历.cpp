d0d40ef2a52424f180d52262099051ad354cf53a
Sweetli1415926 <35253724@qq.com>
2022-04-16 20:27:20 +0800

    2022.04.16

--- 第五章.树与二叉树/3.二叉树的遍历.cpp
+++ 第五章.树与二叉树/3.二叉树的遍历.cpp
@@ -1,21 +1,77 @@
 #include <stdio.h>
-typedef int ElemType;
+#include <malloc.h>
 typedef struct BiTNode
 {
-    ElemType data;
+    char data;
     BiTNode *lchild, *rchild;
 } BiTNode, *BiTree;
-typedef int ElemType;
+typedef BiTree BElemType;
+#pragma region
 #define MaxSize 50
 typedef struct
 {
-    ElemType data[MaxSize];
+    BElemType data[MaxSize];
+    int front, rear;
+} SqQueue;
+
+void InitQueue(SqQueue &Q)
+{
+    Q.rear = Q.front = 0;
+}
+bool isEmpty(SqQueue &Q)
+{
+    if (Q.rear == Q.front)
+        return true;
+    else
+        return false;
+}
+bool EnQueue(SqQueue &Q, BElemType x)
+{
+    if ((Q.rear + 1) % MaxSize == Q.front)
+        return false;
+    Q.data[Q.rear] = x;
+    Q.rear = (Q.rear + 1) % MaxSize;
+    return true;
+}
+bool DeQueue(SqQueue &Q, BElemType &x)
+{
+}
+
+typedef int ElemType;
+
+typedef struct
+{
+    BElemType data[MaxSize];
     int top;
 } SqStack;
+bool IsEmpty(SqStack s)
+{
+    if (s.top == -1)
+    {
+        return true;
+    }
+    return false;
+}
+bool Push(SqStack &S, BElemType x)
+{
+    if (S.top == MaxSize - 1)
+        return false;
+    S.data[++S.top] = x;
+    return true;
+}
+bool Pop(SqStack &S, BElemType &x)
+{
+    if (IsEmpty(S))
+        return false;
+    x = S.data[S.top--];
+    return true;
+}
 void InitStack(SqStack &S)
 {
     S.top = -1;
 }
+
+#pragma endregion
 void Visit(BiTree T)
 {
 }
@@ -34,9 +90,9 @@ void InOrder(BiTree T)
 {
     if (T != NULL)
     {
-        PostOrder(T->lchild);
+        InOrder(T->lchild);
         Visit(T);
-        PostOrder(T->rchild);
+        InOrder(T->rchild);
     }
 }
 // 3.后序遍历
@@ -49,17 +105,124 @@ void PostOrder(BiTree T)
         Visit(T);
     }
 }
-// 1-1.先序遍历的非递归实现
+// 1-2.先序遍历的非递归实现
 void PreOrder2(BiTree T)
+{
+    SqStack S;
+    S.top = -1;
+    InitStack(S);
+    BiTree p = T;
+    while (p || !IsEmpty(S))
+    {
+        if (p) //树不空
+        {
+            Visit(p);
+            Push(S, p);
+            p = p->lchild;
+        }
+        else
+        {              //树空
+            Pop(S, p); //
+            p - p->rchild;
+        }
+    }
+}
+// 2-2.中序遍历的非递归实现
+void InOrder2(BiTree T)
 {
     SqStack S;
     InitStack(S);
     BiTree p = T;
     while (p || !IsEmpty(S))
     {
+        if (p)
+        {
+            Push(S, p);
+            p = p->lchild;
+        }
+        else
+        {
+            Pop(S, p);
+            Visit(p);
+            p = p->rchild;
+        }
+    }
+}
+//层次遍历
+void LevelOrder(BiTree T)
+{
+    SqQueue Q;
+    InitQueue(Q);
+    BiTree p;
+    EnQueue(Q, T); //根结点入队
+    while (!isEmpty(Q))
+    {
+        DeQueue(Q, p);
+        Visit(p);
+        if (p->lchild != NULL)
+            EnQueue(Q, p->lchild);
+        if (p->rchild != NULL)
+            EnQueue(Q, p->rchild);
+    }
+}
+typedef struct ThreadNode //线索二叉树的存储结构
+{
+    ElemType data;
+    struct ThreadNode *lchild, *rchild;
+    int ltag, rtag;
+} ThreadNode, *ThreadTree;
+void InThread(ThreadTree &p, ThreadTree &pre)
+{
+    if (p != NULL)
+    {
+        InThread(p->lchild, pre); //递归,线索化左子树
+        if (p->lchild == NULL)
+        {
+            p->lchild = pre;
+            p->ltag = 1;
+        }
+        if (pre != NULL && pre->rchild == NULL) // pre!=NULL就是说pre不是头结点
+        {
+            pre->rchild = p;
+            pre->rtag = 1;
+        }
+        pre = p;
+        InThread(p->rchild, pre);
     }
 }
 int main()
 {
+
+    BiTree A = (BiTree)malloc(sizeof(BiTree));
+    A->data = 'A';
+    BiTNode *B = (BiTNode *)malloc(sizeof(BiTNode));
+    B->data = 'B';
+    BiTNode *C = (BiTNode *)malloc(sizeof(BiTNode));
+    C->data = 'C';
+    BiTNode *D = (BiTNode *)malloc(sizeof(BiTNode));
+    D->data = 'D';
+    BiTNode *E = (BiTNode *)malloc(sizeof(BiTNode));
+    E->data = 'E';
+    A->lchild = B;
+    A->rchild = C;
+    B->rchild = D;
+    C->lchild = E;
+    void *pre = A;
+
+    ThreadTree A = (ThreadTree)malloc(sizeof(ThreadNode));
+    A->data = 'A';
+    ThreadTree B = (ThreadTree)malloc(sizeof(ThreadNode));
+    B->data = 'B';
+    ThreadTree C = (ThreadTree)malloc(sizeof(ThreadNode));
+    C->data = 'C';
+    ThreadTree D = (ThreadTree)malloc(sizeof(ThreadNode));
+    D->data = 'D';
+    ThreadTree E = (ThreadTree)malloc(sizeof(ThreadNode));
+    E->data = 'E';
+    A->lchild = B;
+    A->rchild = C;
+    B->rchild = D;
+    C->lchild = E;
+    // ThreadTree *pre->next = A
     return 0;
 }
